ecc4b6de36b05a4b8fcd003545e3c6163e75e3c6
Drew Mclean <drew.mclean@effectiveui.com>
2014-06-27 14:29:49 -0600

    cleanup

--- VENTokenField/VENTokenField.m
+++ VENTokenField/VENTokenField.m
@@ -21,12 +21,6 @@
 // THE SOFTWARE.
 
 #import "VENTokenField.h"
-
-<<<<<<< HEAD
-//#import <FrameAccessor/FrameAccessor.h>
-=======
-#import <FrameAccessor/FrameAccessor.h>
->>>>>>> parent of df228c7... pulled out reference to FrameAccessor.
 #import "VENToken.h"
 #import "VENBackspaceTextField.h"
 
@@ -104,13 +98,8 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 {
     [self.collapsedLabel removeFromSuperview];
     self.scrollView.hidden = YES;
-<<<<<<< HEAD
     [self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.originalHeight)];
     
-=======
-    [self setHeight:self.originalHeight];
-
->>>>>>> parent of df228c7... pulled out reference to FrameAccessor.
     CGFloat currentX = 0;
 
     [self layoutToLabelInView:self origin:CGPointMake(self.horizontalInset, self.verticalInset) currentX:&currentX];
@@ -191,12 +180,8 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 
 - (void)layoutCollapsedLabelWithCurrentX:(CGFloat *)currentX
 {
-<<<<<<< HEAD
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(*currentX, CGRectGetMinY(self.toLabel.frame), self.frame.size.width - *currentX - self.horizontalInset, self.toLabel.frame.size.height)];
     
-=======
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(*currentX, CGRectGetMinY(self.toLabel.frame), self.width - *currentX - self.horizontalInset, self.toLabel.height)];
->>>>>>> parent of df228c7... pulled out reference to FrameAccessor.
     label.font = [UIFont fontWithName:@"HelveticaNeue" size:15.5];
     label.text = [self collapsedText];
     label.textColor = self.colorScheme;
@@ -210,12 +195,10 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 {
     [self.toLabel removeFromSuperview];
     self.toLabel = [self toLabel];
-<<<<<<< HEAD
-//    self.toLabel.origin = origin;
-    self.toLabel.frame = CGRectMake(origin.x, origin.y, self.toLabel.frame.size.width, self.toLabel.frame.size.height);
-=======
-    self.toLabel.origin = origin;
->>>>>>> parent of df228c7... pulled out reference to FrameAccessor.
+    self.toLabel.frame = CGRectMake(origin.x,
+                                    origin.y,
+                                    self.toLabel.frame.size.width,
+                                    self.toLabel.frame.size.height);
     [view addSubview:self.toLabel];
     *currentX += self.toLabel.hidden ? CGRectGetMinX(self.toLabel.frame) : CGRectGetMaxX(self.toLabel.frame) + VENTokenFieldDefaultToLabelPadding;
 }
@@ -235,18 +218,21 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
         [token setTitleText:[NSString stringWithFormat:@"%@,", title]];
         [self.tokens addObject:token];
 
-        if (*currentX + token.width <= self.scrollView.contentSize.width) { // token fits in current line
-            token.frame = CGRectMake(*currentX, *currentY, token.width, token.height);
+        if (*currentX + token.frame.size.width <= self.scrollView.contentSize.width) { // token fits in current line
+            token.frame = CGRectMake(*currentX,
+                                     *currentY,
+                                     token.frame.size.width,
+                                     token.frame.size.height);
         } else {
-            *currentY += token.height;
+            *currentY += token.frame.size.height;
             *currentX = 0;
-            CGFloat tokenWidth = token.width;
+            CGFloat tokenWidth = token.frame.size.width;
             if (tokenWidth > self.scrollView.contentSize.width) { // token is wider than max width
                 tokenWidth = self.scrollView.contentSize.width;
             }
-            token.frame = CGRectMake(*currentX, *currentY, tokenWidth, token.height);
+            token.frame = CGRectMake(*currentX, *currentY, tokenWidth, token.frame.size.height);
         }
-        *currentX += token.width + self.tokenPadding;
+        *currentX += token.frame.size.width + self.tokenPadding;
         [self.scrollView addSubview:token];
     }
 }
@@ -286,10 +272,6 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 {
     if (currentY + [self heightForToken] > CGRectGetHeight(self.frame)) { // needs to grow
         if (currentY + [self heightForToken] <= self.maxHeight) {
-            
-            
-//            [self setHeight:currentY + [self heightForToken]];
-            
             self.frame = CGRectMake(self.frame.origin.x,
                                     self.frame.origin.y,
                                     self.frame.size.width,
@@ -300,7 +282,6 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
                                     self.frame.origin.y,
                                     self.frame.size.width,
                                     self.maxHeight);
-            
         }
     } else { // needs to shrink
         if (currentY + [self heightForToken] > self.originalHeight) {
